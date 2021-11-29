9e2960e468102879698c17a7ba9dbb7346138018
Your kiran <kiran@example.com>
2021-11-29 01:09:20 -0800

    updates

--- admin.sh
+++ admin.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
-# Calculate the sum of two integers with pre initialize values
-# in a shell script
- 
-a=10
-b=20
- 
-sum=$(( $a + $b ))
- 
-echo "Sum is: $sum"
+pipeline {
+    agent any
+
+    stages {
+        stage('Hello') {
+            steps {
+                echo 'Hello World'
+            }
+        }
+    }
+} 
